3cf871872ebcd92db0594a7080f6a60db86cddd0
benBen <benmbenjamin@gmail.com>
2018-02-05 18:20:05 -0800

    render fugly quizzes

--- src/components/QuizzesView.js
+++ src/components/QuizzesView.js
@@ -8,7 +8,17 @@ export default class QuizzesView extends Component {
         <h2>{this.props.quizzes.loading ? 'loading' : ''}</h2>
         {
           this.props.quizzes.queue ?
-          this.props.quizzes.queue.map(q => q) : null
+          this.props.quizzes.queue.map((q,i) => {
+            return(
+              <div className="application" key={i}>
+                <h2>{q.status} - {q.email}</h2>
+                <h4>{q.dob}</h4>
+                {q.photos.filter(p => p).map((p,i) => <img key={i} width="200px" height="auto" src={p.url} />)}
+                <p>{q.freeform}</p>
+                <p>{q.vipCode || null} {q.referer ? <span>{q.referer.fullName} <img src={q.referer.imageUrl} /></span> : null}</p>
+              </div>
+            )
+          }) : null
         }
       </div>
     )
--- src/reducers/quizzes.js
+++ src/reducers/quizzes.js
@@ -4,11 +4,16 @@ const initialState = {}
 export default (state=initialState, action) => {
   switch (action.type) {
     case 'quizzes.fetch':
-      setTimeout(() => {
-          store.dispatch({type: 'quizzes.dump', payload: ['a', 'b', 'c']})
-      }, 3000)
+      api('/applications', {
+        method: 'GET',
+        accessToken: 'c1b982a0-06f3-11e8-914e-b5b6fc997f54',
+      }).then(d => store.dispatch({
+        type: 'quizzes.dump',
+        payload: d.applications
+      }))
       return {state, loading: true}
     case 'quizzes.dump':
+      console.log(action.payload)
       return {state, loading: false, queue: action.payload}
     default:
       return state
